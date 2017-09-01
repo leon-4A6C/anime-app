f01bbc604b6ff5912d0562e9ea2e4cbad45ddae1
leon in 't Veld <leon3110l@gmail.com>
2017-09-01 14:27:00 +0200

    added an update button

--- app/screens/settings.js
+++ app/screens/settings.js
@@ -3,24 +3,77 @@ import {
     View,
     Text,
     StyleSheet,
+    Button,
+    ToastAndroid
 } from "react-native"
+import { connect } from "react-redux";
+import { update } from "../actions"
 
 import uiTheme from "../uiTheme"
 
-export default class Settings extends React.Component {
+class Settings extends React.Component {
+
+    componentDidUpdate(prevProps) {
+
+        console.log(this.props.update)
+
+        if(this.props.update.isFetching) {
+            ToastAndroid.show('checking for updates...', ToastAndroid.SHORT); 
+        }
+
+        if(prevProps.update.isFetching && !this.props.update.isFetching) {
+            if(this.props.update.update) {
+                ToastAndroid.show('downloading update', ToastAndroid.SHORT);    
+            } else {
+                ToastAndroid.show("no new update found :(", ToastAndroid.SHORT);
+            }
+        }
+
+    }
+
+    update() {
+        this.props.checkUpdate();
+    }
+
     render() {
         return (
-            <View>
-                <Text style={styles.text}>Sorry no settings yet</Text>
+            <View style={styles.container}>
+                <View style={styles.updateButton}>
+                    <Button
+                        onPress={this.update.bind(this)}
+                        title="update"
+                        color={uiTheme.palette.secundaryColor}
+                        accessibilityLabel="check and download updates"
+                    />
+                </View>
             </View>
         );
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        update: state.update,
+    }
+}
+function mapActionToProps(dispatch) {
+    return {
+        checkUpdate: () => dispatch(update.update()),
+    }
+}
+
+export default connect(mapStateToProps, mapActionToProps)(Settings);
+
 const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
     text: {
         fontWeight: "bold",
         fontSize: uiTheme.text.fontSize+3,
         textAlign: "center"
+    },
+    updateButton: {
+        height: 50,
     }
 })
